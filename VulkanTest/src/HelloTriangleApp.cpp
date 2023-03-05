f3ca46948340294f654d6d75c5dc4a5256a8ab74
rcdavis <rcdavisgd@gmail.com>
2023-03-04 23:54:02 -0500

    Added window

--- VulkanTest/src/HelloTriangleApp.cpp
+++ VulkanTest/src/HelloTriangleApp.cpp
@@ -1,5 +1,30 @@
 #include "HelloTriangleApp.h"
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+
+HelloTriangleApp::~HelloTriangleApp()
+{
+    Cleanup();
+}
+
+void HelloTriangleApp::InitWindow()
+{
+    if (!glfwInit())
+    {
+        throw std::runtime_error("Failed to init GLFW");
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    mWindow = glfwCreateWindow(WindowWidth, WindowHeight, "Vulkan Test", nullptr, nullptr);
+    if (!mWindow)
+        throw std::runtime_error("Failed to create GLFW window");
+}
+
 void HelloTriangleApp::InitVulkan()
 {
 
@@ -7,10 +32,16 @@ void HelloTriangleApp::InitVulkan()
 
 void HelloTriangleApp::MainLoop()
 {
-
+    while (!glfwWindowShouldClose(mWindow))
+    {
+        glfwPollEvents();
+    }
 }
 
 void HelloTriangleApp::Cleanup()
 {
+    glfwDestroyWindow(mWindow);
+    mWindow = nullptr;
 
+    glfwTerminate();
 }
--- VulkanTest/src/HelloTriangleApp.h
+++ VulkanTest/src/HelloTriangleApp.h
@@ -1,17 +1,32 @@
 #pragma once
 
+#include <cstdint>
+
+struct GLFWwindow;
+
 class HelloTriangleApp
 {
 public:
+    HelloTriangleApp() = default;
+    ~HelloTriangleApp();
+
     void Run()
     {
+        InitWindow();
         InitVulkan();
         MainLoop();
         Cleanup();
     }
 
 private:
+    void InitWindow();
     void InitVulkan();
     void MainLoop();
     void Cleanup();
+
+private:
+    GLFWwindow* mWindow = nullptr;
+
+    static constexpr uint32_t WindowWidth = 800;
+    static constexpr uint32_t WindowHeight = 600;
 };
